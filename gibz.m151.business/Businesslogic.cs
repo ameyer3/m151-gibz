55c768d384298134f1e78827d8f194d7f3ec4f8f
Alina Meyer <ameyer3@online.gibz.ch>
2021-06-11 10:41:54 +0200

    add editing and deleting of persons

--- gibz.m151.business/Businesslogic.cs
+++ gibz.m151.business/Businesslogic.cs
@@ -26,5 +26,35 @@ namespace gibz.m151.business
         {
             return dbContext.Person.ToList<Person>();
         }
+
+        public static void DeletePerson(Person person)
+        {
+            Person onePerson = dbContext.Person.Find(person.Id);
+            dbContext.Person.Remove(onePerson);
+            dbContext.SaveChanges();
+        }
+        public static Person ShowPersonToEdit(int id)
+        {
+            Person person = dbContext.Person.Find(id);
+            return person;
+        }
+
+        public static void EditPerson(Person person)
+        {
+            if (person.Id > 0)
+            {
+                //Das Person Objekt wird manuell neu zogeordnet damit es modifiziert gespeichert werden kann
+                Person restaurantFromdbContext = dbContext.Person.Find(person.Id);
+                restaurantFromdbContext.Name = person.Name;
+                restaurantFromdbContext.Age = person.Age;
+                restaurantFromdbContext.Married = person.Married;
+            }
+            else
+            {
+                dbContext.Person.Add(person);
+            }
+            dbContext.SaveChanges();
+
+        }
     }
 }
